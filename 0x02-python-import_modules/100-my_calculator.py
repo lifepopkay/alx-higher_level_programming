cc65ec7bcd6f3a3f8c828e43985cea07ae593203
lifepopkay <popoolafolas@gmail.com>
2023-12-03 02:48:30 -0800

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,21 +6,21 @@ if __name__ == "__main__":
 
     if len(argv) == 4:
         a = int(argv[1])
-        b = int(argv[3])
         op = argv[2]
+        b = int(argv[3])
 
         if op != '+' and op != '-' and op != '*' and op != '/':
             print('Unknown operator. Available operators: +, -, * and /')
             sys.exit(1)
         else:
             if op == '+':
-                print(f"{a} {op} {b} = {add(a,b)}")
+                print(f"{a} {op} {b} = {a + b}")
             elif op == '*':
-                print(f"{a} {op} {b} = {mul(a,b)}")
+                print(f"{a} {op} {b} = {a * b}")
             elif op == '/':
-                print(f"{a} {op} {b} = {div(a,b)}")
+                print(f"{a} {op} {b} = {a / b}")
             else:
-                print(f"{a} {op} {b} = {sub(a,b)}")
+                print(f"{a} {op} {b} = {a - b}")
     else:
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
