2ddf9270fd6b170c326f9a0118bd36d66c040967
lifepopkay <popoolafolas@gmail.com>
2023-12-05 16:12:33 -0800

    error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(0, len(matrix)):
@@ -9,4 +8,4 @@ def print_matrix_integer(matrix=[[]]):
                 else:
                     print("{:d}".format(matrix[i][j]))
     else:
-        print ()
+        print()
