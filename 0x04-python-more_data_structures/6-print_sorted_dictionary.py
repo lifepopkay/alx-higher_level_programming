dd60ed09e7f893adb240931688fec95ae79ea628
lifepopkay <popoolafolas@gmail.com>
2023-12-08 05:05:45 -0800

    task do

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print(f'{key} : {value}')
+    list_key = list(a_dictionary.keys())
+    list_key.sort()
+    for key in list_key:
+        print(f'{key}: {a_dictionary.get(key)}')
