8b4c09a0427007c1e8fcd6b2cfae69d4d70e3af3
lifepopkay <popoolafolas@gmail.com>
2023-12-29 09:16:26 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" This class set and get """
+
+
+class Square:
+    """
+        Square: defines a square.
+        Attributes:
+            size (no type or value identification): size of square.
+        Method:
+                __init__ : init of size attribute for each instance.
+    """
+
+    def __init__(self, size=0):
+
+        """ Initialization of attributes for instances
+            Args:
+                size (int): size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+
+        """ This is getter gets size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """ This set the value"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an interger")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+
+        """This area of the value"""
+
+        return self.__size ** 2
