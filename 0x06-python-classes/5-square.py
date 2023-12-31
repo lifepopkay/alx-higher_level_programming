a5da1702bc9842eb7fb1ecd89f617a9de9a2b5de
lifepopkay <popoolafolas@gmail.com>
2023-12-30 18:30:22 -0800

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,61 +1,51 @@
 #!/usr/bin/python3
 
 
+
+#!/usr/bin/python3
 class Square:
+    """ A class that defines a square by its size
     """
-        Square: defines a square.
-        Attributes:
-            size (no type or value identification): size of square.
-        Method:
-                __init__ : init of size attribute for each instance.
-    """
-
     def __init__(self, size=0):
-
-        """ Initialization of attributes for instances
-            Args:
-                size (int): size of the square.
+        """ Method to initialize the square object
         """
-
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
+
     @property
     def size(self):
-
-        """ This is getter gets size"""
-
+        """ Method to returns the size value
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-
-        """ This set the value"""
-
+        """ Method to set the size value of the square object
+        """
         if not isinstance(value, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
-    def area(self):
-
-        """This area of the value"""
-
-        return self.__size ** 2
-
     def my_print(self):
-
-        """Print the square with charater #"""
+        """ Method that prints a # square according
+        to the size value
+        """
         if not self.__size:
             print()
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("#", end="")
+                    print("#", end='')
                 print()
