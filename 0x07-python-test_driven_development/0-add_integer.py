754aeb409b9e95ad7285a32877f4606843b0f7e8
lifepopkay <popoolafolas@gmail.com>
2024-01-09 03:31:00 -0800

    tast

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
+"""
+This module adds number only
 
 """
-    0-add_integer: add_integer()
-"""
-def add_integer(a, b=98):
-    	"""Function that adds two integer and/or float numbers
 
-	Args:
-		a: first number
-		b: second number
 
-	Returns:
-        	The addition of the two given numbers
+def add_integrer(a, b=98):
+    """Function to add  two numbers
+    Args:
+        a: first number
+        b:second number
+    Returns:
+        The addition of the two numbers
+    Raises:
+        TypeError: if not integer
 
-	Raises:
-        	TypeError: If a or b aren't integer and/or float numbers
-
-	"""
-
-    	if ((not isinstance(a, int) and not isinstance(a, float))):
-		raise TypeError("a must be an integer")
-	if ((not isinstance(b, int) and not isinstance(b, float))):
-        	raise TypeError("b must be an integer")
-	return (int(a) + int(b))
+    """
+    if not isinstance(a, int) and not isinstance(b, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
