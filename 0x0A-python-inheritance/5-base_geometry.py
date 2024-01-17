8e8f8e57d3e2b9086006fcfde60ff14e52b2392f
lifepopkay <popoolafolas@gmail.com>
2024-01-17 04:45:11 -0800

    task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    BaseGeometry: an empty class
+"""
+
+
+class BaseGeometry():
+    """
+        this is a empty class
+
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    BaseGeometry: an empty class
+"""
+
+
+class BaseGeometry():
+    """
+        this is a empty class
+
+    """
+    def area(Self):
+        """
+            this raise an exception
+
+        """
+        raise Exception("area() is not implemented")
