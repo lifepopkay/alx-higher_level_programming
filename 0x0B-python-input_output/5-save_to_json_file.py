221b41c933e72b9d10d8df01b83ea6daa78ddc8e
lifepopkay <popoolafolas@gmail.com>
2024-01-19 21:39:25 -0800

    task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
             filename: name of file
 
     """
-    with open('filename', 'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
