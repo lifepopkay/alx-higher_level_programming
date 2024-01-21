d01d7477835fd548d84ba7638e78077e57f2218f
lifepopkay <popoolafolas@gmail.com>
2024-01-21 07:23:36 +0000

    task

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,5 +5,12 @@
 
 
 
-
 def class_to_json(obj):
+   """
+      function convert class attributes to json
+      Args:
+           obj: this instance of a class
+      Return:
+           Returns the dictionary description with simple data structure
+    """
+    return obj.__dict__    
