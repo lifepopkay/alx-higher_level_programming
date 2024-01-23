f04be9511a4e4b926ec137bc943cdca9d820409b
lifepopkay <popoolafolas@gmail.com>
2024-01-23 02:30:22 -0800

    task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Class Student
+"""
+
+
+class Student:
+    """
+        Method:
+                Defines a students
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            initialize the attributes of the class
+
+            Args:
+                first_name: The student's first name
+                last_name: THe student's last name
+                age: THe student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(i) == str for i in attrs)):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
+    def reload_from_json(self, json):
+        """
+            Replaces all the attributes of the student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
