9c44816c6ea22b8fd9d933870b88d4ac3e0f3751
lifepopkay <popoolafolas@gmail.com>
2024-01-25 13:48:12 +0000

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,7 +12,7 @@ class Base:
     """
     __nb_objects = 0
 
-    def __init__(slef, id=None):
+    def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
