52b14d85bf4294f233135d301eae60653cd5582a
lifepopkay <popoolafolas@gmail.com>
2024-01-26 02:02:35 -0800

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,6 @@ class Rectangle(Base):
 
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return f'[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x, self.__y, self.__width, self.__height)
