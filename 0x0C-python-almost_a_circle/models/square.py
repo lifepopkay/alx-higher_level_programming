61478b4722652298c7ab87eecc2736d9e55b0964
lifepopkay <popoolafolas@gmail.com>
2024-01-26 04:13:58 -0800

    task

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square: inherit from rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
