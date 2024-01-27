93e1ea1fc08b77e859797403391b7e4cd4e76316
lifepopkay <popoolafolas@gmail.com>
2024-01-26 20:59:57 -0800

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,6 +45,6 @@ class Base:
     def from_json_string(json_string):
         """JSON string to dictionary"""
         if json_string is None or len(json_string) == 0 or json_string == []:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
