91bff1e4fc350b2b54996d2fb6fe5dd59a7463fd
lifepopkay <popoolafolas@gmail.com>
2024-01-26 20:03:25 -0800

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Dictionary to JSON string"""
+        if list_dictionaries is not None or list_dictionaries == []:
+            return json.dumps(list_dictionaries)
+        else:
+            return []
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Json string to file"""
+
+        filename = f"{cls.__name__}.json"
+
+        string = []
+        if not list_objs:
+            pass
+        else:
+            for line in range(len(list_objs)):
+                string.append(list_objs[i].to_dictionary())
+
+        l_d = cls.to_json_string(string)
+
+        with open(filename, 'w') as f:
+            f.write(l_d)
