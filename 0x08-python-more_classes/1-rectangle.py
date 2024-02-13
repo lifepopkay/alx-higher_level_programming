8d9caaf41bdf60f088a9aa4e1f0197b7113f82c4
kayode popoola <75029917+lifepopkay@users.noreply.github.com>
2024-02-13 09:52:48 +0100

    Update 1-rectangle.py
    adjusted

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,9 @@ class Rectangle():
 
     """
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
+        
 
     @property
     def width(self):
