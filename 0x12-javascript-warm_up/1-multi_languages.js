c36fa6b2277ff16aaae88d8c1c0693e02879dde4
lifepopkay <popoolafolas@gmail.com>
2024-04-24 01:57:36 -0700

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// a script that prints 3 lines
+
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
