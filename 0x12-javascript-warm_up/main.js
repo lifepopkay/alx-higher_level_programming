dd2ff480b3c313bfd44a79f98d8e27849ac145ff
lifepopkay <popoolafolas@gmail.com>
2024-04-24 04:39:08 -0700

    advance tasks

--- 0x12-javascript-warm_up/main.js
+++ 0x12-javascript-warm_up/main.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+callMeMoby(3, function () {
+  console.log('C is fun');
+});
