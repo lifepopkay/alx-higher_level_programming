676816406120fcc3d8766e086395cf4ca291a394
lifepopkay <popoolafolas@gmail.com>
2024-05-02 06:01:34 -0700

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    list name of states and cities
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    sql = """SELECT cities.name
+           FROM states
+           JOIN cities
+           ON states.id = cities.id;
+           WHERE states.name = {}""".format(sys.argv[4])
+
+    cur.execute(cur)
+    result = cur.fetchall()
+    for line in result:
+        print(line)
+
+    cur.close()
+    db.close()
