edf7a9209c8ea9a8de866c3bea37babbe2714943
lifepopkay <popoolafolas@gmail.com>
2024-05-05 13:14:06 -0700

    task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 """
-    A script that lists all city objects from the database hbtn_0e_6_usa
-    Username, password and dbname wil be passed as arguments to the script.
+Prints all City objects from the database hbtn_0e_14_usa
 """
-
-
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-
-    # create a session
     session = Session()
 
-    # extract the city
-    city_all = session.query(State, City) \
-                  .filter(State.id==City.state_id).all()
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
 
-
-    for state, city in city_all:
+    for state, city in st_cty:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
