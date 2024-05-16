4c396462caa64375ed0aaaaa02cb875ba5ff14b0
lifepopkay <popoolafolas@gmail.com>
2024-05-16 04:47:49 -0700

    task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script that takes a letter,
+sends a POST request to http://0.0.0.0:5000/search_user,
+and displays the id and namefrom the response if it's valid JSON.
+Uses the requests package and adheres to the specified constraints.
+"""
+
+import sys
+import requests
+import json
+
+
+def search_api(letter):
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': letter}
+
+    try:
+        req = requests.post(url, data=payload)
+        json_req = req.json()
+        if json_req:
+            print("[{}] {}".format(json_req.get('id'),
+                                   json_req.get('name')))
+        else:
+            print("No result")
+    except json.JSONDecodeError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        char = sys.argv[1]
+    else:
+        char = ""
+
+    search_api(char)
