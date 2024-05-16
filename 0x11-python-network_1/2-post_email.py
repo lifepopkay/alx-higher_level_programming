2ccc03f6720392b62070f059aa2ec5164d77120d
lifepopkay <popoolafolas@gmail.com>
2024-05-16 01:05:24 -0700

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    post an email
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    mail = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(mail).encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print('{}'.format(page))
