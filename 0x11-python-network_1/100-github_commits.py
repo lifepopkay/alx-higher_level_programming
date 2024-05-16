2cbdf996251d26948995ab340206f26ae773600b
lifepopkay <popoolafolas@gmail.com>
2024-05-16 07:21:11 -0700

    task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,24 +3,19 @@
 Python script that shows the last 10 commits of a repository
 in GitHub
 """
+from requests import get, auth
+import sys
 
-import requests
-from sys import argv
-
-
-def fetch_users(repo_name, acct_name):
-    url = f'https://api.github.com/repos/{repo_name}/{acct_name}/commits'
-    r = requests.get(url)
-    r_json = r.json()
-    
-    # Get the first 10 commits
-    count = 1
-    for commit in r_json:
-        if count < 11:
-            count += 1
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f'{sha}: {author_name}')
 
 if __name__ == "__main__":
-    fetch_users(argv[1], argv[2])
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        r = get(url)
+        json_o = r.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
+    except:
+        pass
