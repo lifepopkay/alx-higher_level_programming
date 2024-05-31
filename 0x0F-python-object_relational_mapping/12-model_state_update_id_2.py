76d7e1f8e303f80ccfc0d289f2ffb6ff0fdae37b
lifepopkay <popoolafolas@gmail.com>
2024-05-31 02:10:16 -0700

    aaa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,28 +1,42 @@
-
 #!/usr/bin/python3
-"""
-Changes the name of a State object from the database hbtn_0e_6_usa
-"""
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from model_state import Base, State
-    from sys import argv
+    """
+    Script that takes 3 arguments: username, password, and database name
+    and updates the name of a state object with id=2.
+    """
 
-    if (len(argv) != 4):
-        print('Use: username, password database_name')
-        exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database name>".format(sys.argv[0]))
+        sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    username, password, database_name = sys.argv[1:]
 
+    # Connect to the MySQL database
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}'
+    )
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).where(State.id == 2)\
-        .update({'name': 'New Mexico'})
-    session.commit()
+    # Get the state object with id=2 (or None if not found)
+    state = session.query(State).filter_by(id=2).first()
+
+    # Update the state name if it exists
+    if state:
+        state.name = "New Mexico"
+        session.commit()  # Commit changes to the database
+        print("State with id {} updated".format(state.id))
+    else:
+        print("State with id 2 not found")
+
+    # Close the session
     session.close()
+
