57f84399f8861d16d681e0b2c6dd86c1b5cb4d7b
lifepopkay <popoolafolas@gmail.com>
2024-05-31 02:00:53 -0700

    task

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,13 +4,15 @@
     name of State where id = 2 to New Mexico
     Username, password, dbname will be passed as arguments to the script.
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
 
 if __name__ == '__main__':
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                            sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
