3399e927dd6ffb71ed751fafda195b0850333331
lifepopkay <popoolafolas@gmail.com>
2024-06-09 19:47:02 +0100

    task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Get the contents of a webpage
+// and stores in a file
+
+const { error } = require('console');
+const request = require('request');
+const Url = process.argv[2];
+const file = process.argv[3];
+const fs = require('fs');
+
+
+request(Url, 'utf-8', (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  fs.writeFileSync(file, body);
+});
