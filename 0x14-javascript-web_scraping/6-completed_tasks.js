5c644cd38fb4d445fcd8b41b25600a46957d9f88
lifepopkay <popoolafolas@gmail.com>
2024-06-09 21:46:20 +0100

    task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
-// compute the number of tasks completed
+// computes the number of task
 
 const request = require('request');
-const Url = process.argv[2];
 
-request(Url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+const requestURL = process.argv[2];
+const users = {};
+
+request(requestURL, function (err, response, body) {
+  if (err) {
+    return console.log(err);
   }
-  const result = {}
-  const data = JSON.parse(body);
-  for (let i = 0 < body.length; i++) {
-    if (data[i].completed === true) {
-        if (result[data[i].userId] === undefined) {
-            result[data[i].userId] = 0;
-        }
-        result[data[i].userId]++;
+
+  const pBody = JSON.parse(body);
+
+  for (let i = 0; i < pBody.length; i++) {
+    const tmp = pBody[i].userId;
+    if (pBody[i].completed) {
+      users[tmp] = (users[tmp] || 0) + 1;
     }
-    console.log(result);
   }
+
+  console.log(users);
 });
